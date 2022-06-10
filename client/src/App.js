7f3b442f70b03ea6011860eddc6e316b2223733e
Amrit <AmritMaan2221@gmail.com>
2022-06-11 01:22:50 +0530

    able to direct to psrticular pages if user is present

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,40 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from './pages/Login';
-import Register from './pages/Register';
+import "./App.css";
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-        <Route exact path="/" element={<div>Welcome to APlus</div>} />
-        <Route exact path="/login" element={<Login/>} />
-        <Route exact path="/register" element={<Register/>} />
-          
-        </Routes>
-      </Router>
-    </>
-  );
+	const user = null;
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={<div>Welcome to APlus</div>}
+					/>
+					<Route
+						path="/test"
+						element={
+							user ? (
+								<div>This is a test Route</div>
+							) : (
+								<Navigate to="/login" />
+							)
+						}
+					/>
+					<Route exact path="/login" element={<Login />} />
+					<Route exact path="/register" element={<Register />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Register.css";
 
 function Register() {
-  return (
-    <div>Register</div>
-  )
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const [emailValue, setEmailValue] = useState("");
+	const [passwordValue, setPasswordValue] = useState("");
+	const [confirmPasswordValue, setConfirmPasswordValue] = useState("");
+	const [firstNameValue, setFirstNameValue] = useState("");
+	const [lastNameValue, setLastNameValue] = useState("");
+	const [usernameValue, setUsernameValue] = useState("");
+
+	const navigate = useNavigate();
+
+	const onSignUpClicked = async () => {
+		alert("Sign up not implemented yet");
+	};
+	return (
+		<div className="register_container">
+			<div className="register_wrapper">
+				<h1 className="register_title">CREATE AN ACCOUNT</h1>
+				{errorMessage && (
+					<div className="register_fail">{errorMessage}</div>
+				)}
+				<form className="register_form">
+					<input
+						class="register_input"
+						placeholder="first name"
+						value={firstNameValue}
+						onChange={(e) => setFirstNameValue(e.target.value)}
+					/>
+					<input
+						class="register_input"
+						placeholder="last name"
+						value={lastNameValue}
+						onChange={(e) => setLastNameValue(e.target.value)}
+					/>
+					<input
+						class="register_input"
+						placeholder="username"
+						value={usernameValue}
+						onChange={(e) => setUsernameValue(e.target.value)}
+					/>
+					<input
+						class="register_input"
+						value={emailValue}
+						onChange={(e) => setEmailValue(e.target.value)}
+						placeholder="email"
+					/>
+					<input
+						class="register_input"
+						type="password"
+						value={passwordValue}
+						onChange={(e) => setPasswordValue(e.target.value)}
+						placeholder="password"
+					/>
+					<input
+						class="register_input"
+						placeholder="confirm password"
+						value={confirmPasswordValue}
+						onChange={(e) =>
+							setConfirmPasswordValue(e.target.value)
+						}
+					/>
+					<div className="register_agreement">
+						By creating an account, I consent to the processing of
+						my personal data in accordance with the{" "}
+						<b>PRIVACY POLICY</b>
+					</div>
+					<button
+						className="register_button"
+						disabled={
+							!emailValue ||
+							!passwordValue ||
+							passwordValue !== confirmPasswordValue
+						}
+						onClick={onSignUpClicked}
+					>
+						CREATE
+					</button>
+				</form>
+
+				<p className="register_link" onClick={() => navigate("/login")}>
+					Already have an account? Log In
+				</p>
+			</div>
+		</div>
+	);
 }
 
-export default Register
+export default Register;
