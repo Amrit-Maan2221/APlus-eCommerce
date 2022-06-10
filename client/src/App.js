b01ef9bdfe28b1f43a283a112510cf8ef872e85e
Amrit <AmritMaan2221@gmail.com>
2022-06-10 23:28:11 +0530

    react-router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <h1 className="App">
-      Welcome to APlus
-    </h1>
+    <>
+      <Router>
+        <Routes>
+        <Route exact path="/" element={<div>Welcome to APlus</div>} />
+        <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/register" element={<Register/>} />
+          
+        </Routes>
+      </Router>
+    </>
   );
 }
 
