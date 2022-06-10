8fecb89b1c30c0bf5bbbd9861eccbbdabe58307b
Amrit <AmritMaan2221@gmail.com>
2022-06-10 23:00:07 +0530

    error fixed, path module imported in index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const connectDatabase = require('./dbConnection');
 const cors = require("cors");
+const path = require("path");
 const dotenv = require("dotenv");
 
 dotenv.config();
