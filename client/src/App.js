9998aec837e3f881b86a55686dd4156cb94d17e0
Amrit <AmritMaan2221@gmail.com>
2022-06-11 00:02:10 +0530

    made the login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Register from "./Pages/Register";
-import Login from "./Pages/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Login.css'
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    const [errorMessage, setErrorMessage] = useState('');
+    const [emailValue, setEmailValue] = useState("");
+    const [passwordValue, setPasswordValue] = useState("");
+
+    const navigate = useNavigate();
+
+    const onLogInClicked = async () => {
+        alert('Log in not implemented yet');
+    }
+
+    return (
+        <div className='login_container'>
+            <div className='login_wrapper'>
+                <h1 className='login_title'>LOGIN</h1>
+                {errorMessage && <div className="login_fail">{errorMessage}</div>}
+                <form className='login_form'>
+                    <input class='login_input' value={emailValue}
+                        onChange={e => setEmailValue(e.target.value)} placeholder="someone@gmail.com" />
+                    <input class='login_input' type="password" value={passwordValue}
+                        onChange={e => setPasswordValue(e.target.value)} placeholder="password" />
+                    <button className='login_button' disabled={!emailValue || !passwordValue}
+                        onClick={onLogInClicked}>LOGIN</button>
+                    <p className='login_link' onClick={() => navigate('/forgot-password')}>DO NOT YOU REMEMBER THE PASSWORD?</p>
+                    <p className='login_link' onClick={() => navigate('/register')}>CREATE A NEW ACCOUNT</p>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Login
