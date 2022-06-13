0d061779ea98031473715ac5d00c52b8bb8228ec
Amrit <AmritMaan2221@gmail.com>
2022-06-13 22:46:55 +0530

    missing login route added

--- routes/auth.js
+++ routes/auth.js
@@ -28,7 +28,43 @@ router.post("/register", async (req, res) => {
 	}
 });
 
-/**
+//LOGIN
+router.post("/login", async (req, res) => {
+	try {
+		const user = await User.findOne({ username: req.body.username });
+		!user && res.status(401).json("Wrong User Name");
+		console.log(user);
+
+		const hashedPassword = CryptoJS.AES.decrypt(
+			user.password,
+			"process.env.PASS_SEC"
+		);
+
+		const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+		const inputPassword = req.body.password;
+
+		originalPassword !== inputPassword &&
+			res.status(401).json("Wrong Password");
+
+		const accessToken = jwt.sign(
+			{
+				id: user._id,
+				isAdmin: user.isAdmin,
+			},
+			"process.env.JWT_SEC",
+			{ expiresIn: "3d" }
+		);
+
+		const { password, ...others } = user._doc;
+		res.status(200).json({ ...others, accessToken });
+		res.status(200).json(user);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+/**git
  * Export the router
  * Part 3 OF USING EXPRESS ROUTER
  */
